609525f3708379e61802d8bb25becddcef52320b
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2015-02-09 18:40:51 -0500

    Add infrastructure to automatically populate DB from CSV files

--- src/fairtest_project/testframework/admin.py
+++ src/fairtest_project/testframework/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from testframework.models import Product
+from testframework.models import Competitor
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Competitor)
