9db6b3a4159a8b2ef3473d55a70efca4fbd06f72
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2015-02-09 16:00:13 -0500

    Add some doc

--- src/fairtest_project/testframework/models.py
+++ src/fairtest_project/testframework/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+
 class Product(models.Model):
+    """A product to be sold online
+
+    Attributes:
+        name(str): The name of the product
+        basprice(decimal): The initial price of the product,  which is subject
+                           to modifications based on the location of competitors
+    """
     name = models.CharField(max_length=200)
     baseprice = models.DecimalField(max_digits=15,decimal_places=2,default=0)
 
@@ -12,6 +20,16 @@ class Product(models.Model):
 
 
 class Competitor(models.Model):
+    """A Competitor of the selle.
+
+    Attributes:
+        name(str): The name of the competitor
+        basprice(decimal): The address of the competitor.
+                           to modifications based on the location of competitors
+
+    Note:
+        We need to set a specific format for the addresses.
+    """
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
 
