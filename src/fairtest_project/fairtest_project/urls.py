be4c0a1bdac77e53afb65e60511c28d09d95ccc2
Derek Xingzhou He <derekhe@gmail.com>
2015-02-10 11:11:49 -0500

    Fix static file bug

--- src/fairtest_project/fairtest_project/urls.py
+++ src/fairtest_project/fairtest_project/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = patterns('',
@@ -8,4 +10,4 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^testframework/', include('testframework.urls')),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
