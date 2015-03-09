3627406d83b349be08c5f19819ddf068c619e698
Derek He <derekhe@gmail.com>
2015-03-09 18:39:42 -0400

    Front end for correlation admin

--- src/fairtest_project/correlation/admin.py
+++ src/fairtest_project/correlation/admin.py
@@ -3,9 +3,16 @@ from django.contrib import admin
 from django.http import HttpResponse
 from correlation.dummy_correlation import Correlation
 
+from django.template import Context
+from django.template.loader import get_template
 
 def correlation_view(request):
-    return HttpResponse(repr(Correlation.inputs) + repr(Correlation.outputs));
+    t = get_template('correlation_admin');
+    co = []
+    for i in range(len(Correlation.inputs)):
+        co.append((str(Correlation.inputs[i]), str(Correlation.outputs[i])));
+    print(co)
+    return HttpResponse(t.render(Context({'co' : co})))
 
 
 def get_admin_urls(urls):
