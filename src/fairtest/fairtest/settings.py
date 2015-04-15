3075a5b93e61b05c1aac1c543b63e3d2a31baaca
Derek Xingzhou He <derekhe@gmail.com>
2015-04-15 18:43:46 -0400

    Djangorestframework added

--- src/fairtest/fairtest/settings.py
+++ src/fairtest/fairtest/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'fairtest_api'
+    'fairtest_api',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -82,3 +83,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
--- src/fairtest/fairtest/urls.py
+++ src/fairtest/fairtest/urls.py
@@ -1,10 +1,28 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+from fairtest_api.models import User, Transaction
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'sex', 'zip', 'race')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fairtest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
