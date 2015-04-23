275249199c22df9a02904796dea5566d1c0e4589
Derek Xingzhou He <derekhe@gmail.com>
2015-04-23 03:22:15 -0400

    Add admin for other entries

--- src/fairtest/api/admin.py
+++ src/fairtest/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User, Store
+from .models import User, Store, Competitor, Zipcode
 
 admin.site.register(User)
 admin.site.register(Store)
+admin.site.register(Competitor)
+admin.site.register(Zipcode)
