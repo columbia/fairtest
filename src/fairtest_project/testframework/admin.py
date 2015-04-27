53567fad98dcbcceea3fda08db655671f0a21d6e
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2015-04-26 21:48:45 -0400

    Revome old fairtest_project folder

--- src/fairtest_project/testframework/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from testframework.models import Product
-from testframework.models import Competitor
-from testframework.models import Zipcode
-
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Competitor)
-admin.site.register(Zipcode)
