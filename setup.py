8db8b5756f5dd57d41327d98f7e7cad82c6bf59e
Vaggelis Atlidakis <vatlidak@cs.columbia.edu>
2015-09-09 22:23:58 -0400

    Add setup file for packaging Fairtest
    Use:
      python setup.py install

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='fairtest',
+      version='0.1',
+      description='fairtest',
+      author='Florian Tramer, Vaggelis Atlidakis',
+      author_email='florian.tramer@epfl.ch, vatlidak@cs.columbia.edu',
+      package_dir={'': 'src'},
+      packages=['fairtest', 'fairtest.bugreport', 'fairtest.bugreport.clustering',
+                'fairtest.bugreport.core', 'fairtest.bugreport.statistics',
+                'fairtest.bugreport.trees'],
+      test_suite="tests",
+    )
