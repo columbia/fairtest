f466ebccd898dcfd99d4e6f392041f9cb3ea06f1
Florian Tramer <florian.tramer@epfl.ch>
2015-09-16 15:19:03 +0200

    add separate make_xxx for the two medical tasks

---
+++ src_new_api/fairtest/tests/make_medical_reg.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from fairtest.bugreport import api2 as api
+from fairtest.bugreport.helpers import prepare
+
+from time import time
+import sys
+
+def main(argv=sys.argv):
+
+    if len(argv) != 2:
+        usage(argv)
+
+
+    # Preapre data into FairTest friendly format
+    FILENAME = argv[1]
+    data = prepare.data_from_csv(FILENAME)
+    
+    # Initializing parameters for experiment
+    data['Error'] = abs(data['Error'])
+    TARGET = 'Error'
+    SENS = ['Age']
+    EXPL = []
+
+    # Instanciate the experiment
+    t1 = time()
+    FT1 = api.Experiment(data, SENS, TARGET, EXPL,
+                         random_state=0)
+    # Train the classifier
+    t2 = time()
+    FT1.train()
+
+    # Evaluate on the testing set
+    t3 = time()
+    FT1.test(approx_stats=False)
+
+    # Create the report
+    t4 = time()
+    FT1.report("medical_reg")
+
+    t5 = time()
+
+    print "Instantiation: %.2f, Train: %.2f, Test: %.2f, Report: %.2f"\
+            % ((t2-t1), (t3-t2), (t4-t3), (t5-t4))
+
+def usage(argv):
+    print "Usage:%s <filename>" % argv[0]
+    exit(-1)
+
+if __name__ == '__main__':
+    sys.exit(main())
