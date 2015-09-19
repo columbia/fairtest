982d2be4f3fa7ab88ff309576f164a7d33613dd6
Florian Tramer <florian.tramer@epfl.ch>
2015-09-19 22:39:38 +0200

    quick script to format results for plot

---
+++ src_new_api/fairtest/tests/add_perc.py
@@ -0,0 +1,22 @@
+FILES = ['timing_discovery.csv', 'timing_testing.csv', 'timing_error.csv']
+
+count = 0
+for file in FILES:
+    inf = open(file)
+    data = inf.readlines()
+    inf.close()
+    
+    outf = open(file, 'w')
+    outf.write('#set,train,test,ptrain,ptest\n')
+    for i in range(1, len(data)):
+        split = data[i].strip().split(',')
+        print split
+        dset = split[0]
+        train = float(split[1])
+        test = float(split[2])
+        ptrain = 100*train/(train+test)
+        ptest = 100*test/(train+test)
+        outf.write('{},{},{},{:.2f},{:.2f},{},{}\n'.format(dset, train, test, ptrain, ptest,int(train+test),count))
+        count += 1
+    outf.close()
+    count += 1
